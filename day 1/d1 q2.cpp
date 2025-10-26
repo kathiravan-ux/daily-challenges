5c870d5b0a56aff675f8e9625372052723c3c41e
kathiravan-ux <ananthikathiravan@gmail.com>
2025-10-26 20:24:53 +0530

    Add files via upload

---
+++ day 1/d1 q2.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, welcome to C programming!\n");
+    return 0;
+}
+
---
+++ day 1/d1 q4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float num;  
+
+    printf("Enter a fractional number: ");
+    scanf("%f", &num);  
+
+    printf("You entered: %f\n", num);  
+    return 0;
+}
